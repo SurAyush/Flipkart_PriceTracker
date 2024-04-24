94aaa3b9a9cf7c9d988cf416cdf7c235aafb7916
SurAyush <ayushsur26@gmail.com>
2024-04-24 14:24:51 +0530

    Final change touch before submission

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ from email.mime.image import MIMEImage
 
 # Input email details
 def send_email_func(product,price,url,threshold,imgname,receiver):
-    sender_email = "avisinghal627@gmail.com"
+    sender_email = "projectworksnit@gmail.com"
     receiver_email = receiver
     subject = f"Exciting News: Price Drop Alert for {product}!"
     text = f"Dear Valued Customer,\n We hope this message finds you well. We're delighted to inform you of some fantastic news regarding the product you've shown interest in, {product}.\nAs per your request, we've been closely monitoring the pricing for the product and are thrilled to announce that there has been a significant drop in its price! The current price has fallen below the threshold price of {threshold}, which you provided to us earlier.\nNew price: {price}\nYou can take advantage of this price drop by visiting the following link: {url} \nWe understand how important it is to get the best value for your money, and we're thrilled to be able to offer you this reduced price on {product}.\nThank you for choosing us for your needs. We look forward to serving you again soon!"
@@ -29,7 +29,7 @@ def send_email_func(product,price,url,threshold,imgname,receiver):
     # Connect to the SMTP server
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
-        server.login(sender_email, "eqvcevttodervvfe")  # Use the actual sender's password here
+        server.login(sender_email, "sdujytqxxpvuwpoe")  # Use the actual sender's password here
         server.send_message(message)
 
     print("Email has been sent to " + receiver_email)
